0d164d17fe83f184097cc01d432064e0c65e9f4f
Adrien Roches <adrien@roc-it.tech>
2023-04-11 16:24:57 +0200

    [sfstart/04-encore] Use of Encore

--- src/Model/MovieRepository.php
+++ src/Model/MovieRepository.php
@@ -11,7 +11,7 @@ use function array_key_exists;
 use function array_map;
 
 /**
- * @phpstan-type RawMovieData array{slug: string, title: string, plot: string, releasedAt: string, genres: list<string>}
+ * @phpstan-type RawMovieData array{slug: string, title: string, plot: string, releasedAt: string, poster: string, genres: list<string>}
  */
 final class MovieRepository
 {
@@ -25,6 +25,7 @@ final class MovieRepository
             'plot'       => "Malgré sa paralysie, Jake Sully, un ancien marine immobilisé dans un fauteuil roulant, est resté un combattant au plus profond de son être. Il est recruté pour se rendre à des années-lumière de la Terre, sur Pandora, où de puissants groupes industriels exploitent un minerai rarissime destiné à résoudre la crise énergétique sur Terre. Parce que l'atmosphère de Pandora est toxique pour les humains, ceux-ci ont créé le Programme Avatar, qui permet à des \" pilotes\" humains de lier leur esprit à un avatar, un corps biologique commandé à distance, capable de survivre dans cette atmosphère létale. Ces avatars sont des hybrides créés génétiquement en croisant l'ADN humain avec celui des Na'vi, les autochtones de Pandora.
 Sous sa forme d'avatar, Jake peut de nouveau marcher. On lui confie une mission d'infiltration auprès des Na'vi, devenus un obstacle trop conséquent à l'exploitation du précieux minerai. Mais tout va changer lorsque Neytiri, une très belle Na'vi, sauve la vie de Jake...",
             'releasedAt' => '16/12/2009',
+            'poster'     => 'avatar.webp',
             'genres'     => ['Action', 'Adventure', 'Fantasy'],
         ],
         [
@@ -33,6 +34,7 @@ Sous sa forme d'avatar, Jake peut de nouveau marcher. On lui confie une mission 
             'plot'       => "Cléopâtre, la reine d’Égypte, décide, pour défier l'Empereur romain Jules César, de construire en trois mois un palais somptueux en plein désert. Si elle y parvient, celui-ci devra concéder publiquement que le peuple égyptien est le plus grand de tous les peuples. Pour ce faire, Cléopâtre fait appel à Numérobis, un architecte d'avant-garde plein d'énergie. S'il réussit, elle le couvrira d'or. S'il échoue, elle le jettera aux crocodiles.
 Celui-ci, conscient du défi à relever, cherche de l'aide auprès de son vieil ami Panoramix. Le druide fait le voyage en Égypte avec Astérix et Obélix. De son côté, Amonbofis, l'architecte officiel de Cléopâtre, jaloux que la reine ait choisi Numérobis pour construire le palais, va tout mettre en œuvre pour faire échouer son concurrent.",
             'releasedAt' => '30/01/2002',
+            'poster'     => 'mission-cleopatre.jpg',
             'genres'     => ['Documentary', 'Adventure', 'Comedy', 'Family'],
         ],
     ];
@@ -47,6 +49,7 @@ Celui-ci, conscient du défi à relever, cherche de l'aide auprès de son vieil 
             title: $data['title'],
             plot: $data['plot'],
             releasedAt: DateTimeImmutable::createFromFormat('d/m/Y', $data['releasedAt']),
+            poster: $data['poster'],
             genres: $data['genres'],
         );
     }
