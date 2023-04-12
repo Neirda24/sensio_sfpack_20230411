33f39d2631aac73369990993c245c8a417054602
Adrien Roches <adrien@roc-it.tech>
2023-04-12 16:17:45 +0200

    [sfstart/03-twig-dynamic-navbar] Add class on active menu

--- src/Controller/NavBarController.php
+++ src/Controller/NavBarController.php
@@ -9,12 +9,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class NavBarController extends AbstractController
 {
-    public function __invoke(MovieRepository $movieRepository): Response
+    public function __invoke(MovieRepository $movieRepository, string $currentRoute, ?string $currentSlug): Response
     {
         $movies = Movie::fromEntities($movieRepository->listAll());
 
         return $this->render('navbar.html.twig', [
             'movies' => $movies,
+            'currentRoute' => $currentRoute,
+            'currentSlug' => $currentSlug,
         ]);
     }
 }
