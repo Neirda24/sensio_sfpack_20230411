b5ede20a54ee05834f8beee8fc7323e9cb2a29f8
Adrien Roches <adrien@roc-it.tech>
2023-04-13 12:14:30 +0200

    [sfstart/08-services] Inject poster asset path in Validator instead of static use.

--- src/Validator/Constraints/PosterExistsValidator.php
+++ src/Validator/Constraints/PosterExistsValidator.php
@@ -8,9 +8,19 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 use function file_exists;
+use function rtrim;
 
 final class PosterExistsValidator extends ConstraintValidator
 {
+    private readonly string $posterAssetsPath;
+
+    public function __construct(
+        string $posterAssetsPath
+    )
+    {
+        $this->posterAssetsPath = rtrim($posterAssetsPath, '/');
+    }
+
     public function validate(mixed $value, Constraint $constraint): void
     {
         if (!$constraint instanceof PosterExists) {
@@ -21,12 +31,11 @@ final class PosterExistsValidator extends ConstraintValidator
             return;
         }
 
-        $fullPath = __DIR__ . '/../../../assets/images/movies/' . $value;
+        $fullPath = "{$this->posterAssetsPath}/{$value}";
         if (!file_exists($fullPath)) {
             $violationBuilder = $this->context
                 ->buildViolation($constraint->message)
-                ->setParameter('{{ filename }}', $value)
-            ;
+                ->setParameter('{{ filename }}', $value);
 
             $violationBuilder->addViolation();
         }
