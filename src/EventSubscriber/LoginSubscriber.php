d787aad4a67a854bcc906020046cd839e89b6237
Adrien Roches <adrien@roc-it.tech>
2023-04-14 14:21:25 +0200

    [sfstart/12-events] Subscribe to LoginSuccess to set the last logged in date in User

---
+++ src/EventSubscriber/LoginSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Psr\Clock\ClockInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+class LoginSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly ClockInterface $clock,
+        private readonly UserRepository $userRepository,
+    )
+    {
+    }
+
+    public function updateLastLoggedIn(LoginSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setLastLoggedIn($this->clock->now());
+        $this->userRepository->save($user, true);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            LoginSuccessEvent::class => [
+                ['updateLastLoggedIn', 0]
+            ],
+        ];
+    }
+}
